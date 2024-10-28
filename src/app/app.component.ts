a70ce9c31f412392ad83c78b4db8b1c329c937a0
sarvsav <sarvsav@gmail.com>
2024-10-28 20:12:55 +0100

    feat: showing weeknumber

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CardModule } from 'primeng/card';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [RouterOutlet, CardModule, CommonModule],
+  template: `<div class="card flex justify-content-center">
+  <p-card header="Week number">
+      <p class="m-0">
+        Today is {{today | date}} and week number is {{today | date:'w'}}
+      </p>
+  </p-card>
+</div>`,
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'tools';
+  today: number = Date.now();
 }
