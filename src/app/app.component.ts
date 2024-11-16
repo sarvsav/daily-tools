e78121bb52dc191a4a8fddb71c164c7157e27872
sarvsav <sarvsav@gmail.com>
2024-11-16 13:46:58 +0100

    feat: display go trending repos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CardModule } from 'primeng/card';
 import { CommonModule } from '@angular/common';
+import { JsonReaderService } from './json-reader.service';
 
 @Component({
   selector: 'app-root',
@@ -13,10 +14,72 @@ import { CommonModule } from '@angular/common';
         Today is {{today | date}} and week number is {{today | date:'w'}}
       </p>
   </p-card>
-</div>`,
-  styleUrl: './app.component.scss'
+</div>
+
+<div class="card-container">
+    <div class="card flex justify-content-center" *ngFor="let card of cards">
+        <p-card header="{{card.header}}">
+            <!-- Clipboard icon -->
+            <button class="clipboard-icon" (click)="copyToClipboard(card.header)">
+            <i class="pi pi-clipboard" style="color: green"></i> <!-- Using Material icon -->
+            </button>
+            <!-- Card Content -->
+            <ol class="m-0">
+  <li *ngFor="let entry of card.content"> {{ entry }} </li>
+</ol>
+        </p-card>
+    </div>
+</div>
+
+
+
+`,
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'tools';
+  jsonData: any = {};
   today: number = Date.now();
+
+  cards = [
+    {
+      header: 'Daily',
+      content: []
+    },
+    {
+      header: 'Weekly',
+      content: []
+    },
+    {
+      header: 'Monthly',
+      content: []
+    }
+  ];
+
+  copyToClipboard(content: string): void {
+    navigator.clipboard.writeText(content).then(
+      () => {
+        console.log('Content copied to clipboard');
+        // Optionally, show a success message
+        alert('Content copied to clipboard!');
+      },
+      (err) => {
+        console.error('Could not copy text:', err);
+      }
+    );
+  }
+
+  constructor(private jsonReaderService: JsonReaderService) {}
+  ngOnInit(): void {
+    this.jsonReaderService.getJsonData().subscribe(
+      (data) => {
+        this.cards[0].content = data.data.daily
+        this.cards[1].content = data.data.weekly
+        this.cards[2].content = data.data.monthly
+      },
+      (error) => {
+        console.error('Error loading JSON data:', error);
+      }
+    );
+  }
 }
---
+++ src/app/json-reader.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class JsonReaderService {
+  private jsonFilePath = '/data.json'; // Adjust the filename as needed
+  constructor(private http: HttpClient) { }
+  getJsonData(): Observable<any> {
+    return this.http.get<any>(this.jsonFilePath);
+  }
+}
